2d3a421f9ffec772f69b6623c494a3cb6af1ca55
Aillkeen <aillkeenoliveira@gmail.com>
2018-07-22 22:42:11 -0300

    Criacao do projeto base

---
+++ src/test/java/com/aillkeen/daca/DacaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aillkeen.daca;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DacaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
